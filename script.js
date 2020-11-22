b3b5e95d53b8ce2a07b5ce2a5db88c270b66d2a3
spardutti <luisdamian.sp@gmail.com>
2020-11-22 11:46:55 -0300

    added npc play, wind cond, need to add game loop and minmax AI

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 (function game() {
+  let playerTurn = 0;
   //module
   const Gameboard = (() => {
-    let gameboard = new Array(9);
+    let gameboard = new Array(9).fill("");
 
     return { gameboard };
   })();
@@ -14,35 +15,77 @@
   const player = PlayerFactory("player", "x");
   const computer = PlayerFactory("computer", "o");
 
-  displayGameboard(Gameboard.gameboard, player, computer);
+  //display gameboard
+  function displayGameboard(board) {
+    let contenDiv = document.getElementById("content");
+    let boardContainer = document.createElement("div");
+    boardContainer.setAttribute("class", "board-container");
 
-})();
-//display gameboard
-function displayGameboard(board, player, computer) {
-  let contenDiv = document.getElementById("content");
-  let boardContainer = document.createElement("div");
-  boardContainer.setAttribute("class", "board-container");
+    for (let i = 0; i < board.length; i++) {
+      let divSquare = document.createElement("div");
+      divSquare.setAttribute("id", i);
+      divSquare.setAttribute("class", "div-square");
+      divSquare.addEventListener("click", () => {
+        playerPlay(board);
+      });
+      boardContainer.appendChild(divSquare);
+    }
+    contenDiv.appendChild(boardContainer);
+  }
 
-  for (let i = 0; i < board.length; i++) {
-    let divSquare = document.createElement("div");
-    divSquare.setAttribute("id", i);
-    divSquare.setAttribute("class", "div-square");
-    divSquare.addEventListener("click", () => {
-      playGame(player, computer);
+  displayGameboard(Gameboard.gameboard);
+
+  //game logic
+  function playerPlay(board) {
+    let id = event.target.id;
+    let clickedDiv = document.getElementById(id);
+    if (clickedDiv.innerHTML == "" && playerTurn == 0) {
+      clickedDiv.innerHTML = player.selection;
+      board.splice(id, 1, player.selection);
+      playerTurn = 1;
+      checkWinner(player.selection, board);
+      setTimeout(() => {
+        computerPlay(board);
+      }, 1000);
+    }
+  }
+  //NOT WORKING PROPERLY, will keep generating number after full array
+  function computerPlay(board) {
+    if (board.some((e) => e === "")) {
+      let id = Math.floor(Math.random() * 9);
+      let clickedDiv = document.getElementById(id);
+      while (clickedDiv.innerHTML != "") {
+        let id = Math.floor(Math.random() * 9);
+        clickedDiv = document.getElementById(id);
+      }
+      clickedDiv.innerHTML = computer.selection;
+      board.splice(id, 1, computer.selection);
+      checkWinner(computer.selection, board)
+      playerTurn = 0;
+    } else console.log("all taken");
+  }
+
+  function checkWinner(selection, board) {
+    const vertical = [0, 3, 6].map((i) => {
+      return [i, i + 1, i + 2];
     });
-    boardContainer.appendChild(divSquare);
+    const horitzontal = [0, 1, 2].map((i) => {
+      return [i, i + 3, i + 6];
+    });
+    const diagonal = [
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    const winnerArr = [].concat(vertical).concat(horitzontal).concat(diagonal);
+    let result = winnerArr.some((e) => {
+      return (
+        board[e[0]] == selection &&
+        board[e[1]] == selection &&
+        board[e[2]] == selection
+      );
+    });
+    if (result) {
+      alert("WIN");
+    }
   }
-  contenDiv.appendChild(boardContainer);
-}
-//game logic
-function playGame(player, computer) {
-  let turn = player;
-  let id = event.target.id;
-  let clickedDiv = document.getElementById(id);
-  if (clickedDiv.innerHTML == "") {
-    clickedDiv.innerHTML = turn.selection;
-    turn = computer
-  } else {
-    console.log("next");
-  }
-}
+})();
